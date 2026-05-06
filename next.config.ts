36911193bb9f07247b89a28180720113f95fe455
Diogo <diogomenezesteo@gmail.com>
2026-05-06 17:37:43 -0300

    Forçando uso do Webpack para build

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
-const withPWA = require('next-pwa')({
+import nextPWA from 'next-pwa';
+
+const withPWA = nextPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development'
-})
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
