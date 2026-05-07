8a73010ddf5b000443a09f10e01d2f1f96d33990
Diogo <diogomenezesteo@gmail.com>
2026-05-07 16:26:08 -0300

    Revert type module and stabilize config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 import type { NextConfig } from "next";
-import nextPWA from 'next-pwa';
-
-const withPWA = nextPWA({
+const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development'
-});
+})
 
 const nextConfig: NextConfig = {
   /* config options here */
